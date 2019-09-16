d06f27f922cfd61a884a98923676ec9d0318bf6c
Francis San Filippo <fsanfilipp@vailsys.com>
2019-09-16 14:33:52 -0500

    initial commit

---
+++ listParticipants.js
@@ -0,0 +1,30 @@
+const persephonySDK = require('@persephony/sdk')
+
+const accountId = process.env.accountId
+const authToken = process.env.authToken
+// your Persephony API key (available in the Dashboard) - be sure to set up environment variables to store these values
+const persephony = persephonySDK(accountId, authToken)
+
+// Invoke get participants method
+getConferenceParticipants(conferenceId).then(participants => {
+  // Use participants
+}).catch(err => {
+  // Catch Errors
+})
+
+async function getConferenceParticipants(conferenceId) {
+  // Create array to store all participants
+  const participants = []
+  // Invoke GET method to retrieve initial list of participant information
+  const first = await persephony.api.conferences.participants(conferenceId).getList()
+  participants.push(...first.participants)
+  // Get Uri for next page
+  let nextPageUri = first.nextPageUri
+  // Retrieve entire participant list 
+  while (nextPageUri) {
+    const nextPage = await persephony.api.conferences.participants(conferenceId).getNextPage(nextPageUri)
+    participants.push(...nextPage.participants)
+    nextPageUri = nextPage.nextPageUri
+  }
+  return participants
+}
